97c74a9a17e8218e5aa79bc359dcc2d34aab1939
viryoke <viryoke@gmail.com>
2026-06-08 19:02:54 +0800

    test: add arch-test host for end-to-end verification

--- flake.nix
+++ flake.nix
@@ -53,6 +53,20 @@
             hostname = "macbook";
           };
         };
+
+        "viryoke@arch-test" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.aarch64-linux;
+          modules = [
+            ./hosts/arch-test
+            agenix.homeManagerModules.default
+          ];
+          extraSpecialArgs = {
+            inherit self;
+            isLinux = true;
+            isDarwin = false;
+            hostname = "arch-test";
+          };
+        };
       };
     };
 }
