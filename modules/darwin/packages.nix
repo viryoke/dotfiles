98aa3bfbdc1b71e8dcad3bfdce12bd88b834521e
viryoke <viryoke@gmail.com>
2026-06-08 18:34:57 +0800

    feat: add platform-specific Nix package modules

---
+++ modules/darwin/packages.nix
@@ -0,0 +1,6 @@
+{ pkgs, ... }: {
+  home.packages = with pkgs; [
+    yazi
+    zellij
+  ];
+}
---
+++ modules/shared/packages.nix
@@ -0,0 +1,27 @@
+{ pkgs, lib, isLinux, isDarwin, ... }: {
+  home.packages = with pkgs; [
+    ripgrep
+    fd
+    fzf
+    eza
+    zoxide
+    bat
+    jq
+    curl
+    wget
+    unzip
+    tree
+    htop
+    btop
+    lazygit
+    delta
+    age
+  ] ++ lib.optionals isLinux [
+    wl-clipboard
+    xdg-utils
+  ] ++ lib.optionals isDarwin [
+    m-cli
+    coreutils
+    gnu-sed
+  ];
+}
