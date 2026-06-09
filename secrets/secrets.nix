96d3b6fd074edaccd44168b6173071a5b45987cb
viryoke <viryoke@users.noreply.github.com>
2026-06-09 16:55:02 +0800

    feat: add macbook SSH key to agenix secrets
    - Add viryoke-macbook public key to secrets.nix
    - Re-key clash_subscription.age and github_token.age
    - Both cachyos-desktop and macbook can now decrypt secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,11 +1,8 @@
 let
-  # cachyos-desktop user key
   viryoke-desktop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINDP1zmZLvEp8TTXCr3NLvS7VRUJcV9uXgn/H+Qs3gEH viryoke@cachyos-desktop";
+  viryoke-macbook = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMBJ3saQcwyNpXk+Fu4crDH0tUuSw3liEd0S+gayJ4kH viryoke@macbook";
 
-  # TODO: add macbook key later
-  # viryoke-macbook = "ssh-ed25519 AAAA... viryoke@macbook";
-
-  allKeys = [ viryoke-desktop ];
+  allKeys = [ viryoke-desktop viryoke-macbook ];
 in
 {
   "clash_subscription.age".publicKeys = allKeys;
