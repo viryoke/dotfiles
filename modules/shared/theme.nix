d319e79a451ac74b78f49b2a33924c9f69dd2754
viryoke <viryoke@gmail.com>
2026-06-11 19:40:18 +0800

    fix: remove invalid $DRY_RUN_CMD before for loops in theme.nix

--- modules/shared/theme.nix
+++ modules/shared/theme.nix
@@ -7,10 +7,10 @@
   # macOS: register fonts with the system
   home.activation.registerFonts = lib.mkIf isDarwin (lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     $DRY_RUN_CMD mkdir -p $HOME/Library/Fonts
-    $DRY_RUN_CMD for font in ${pkgs.nerd-fonts.jetbrains-mono}/share/fonts/truetype/*.ttf; do
+    for font in ${pkgs.nerd-fonts.jetbrains-mono}/share/fonts/truetype/*.ttf; do
       $DRY_RUN_CMD ln -sf "$font" "$HOME/Library/Fonts/" 2>/dev/null || true
     done
-    $DRY_RUN_CMD for font in ${pkgs.noto-fonts-cjk-sans}/share/fonts/opentype/noto/*.otf; do
+    for font in ${pkgs.noto-fonts-cjk-sans}/share/fonts/opentype/noto/*.otf; do
       $DRY_RUN_CMD ln -sf "$font" "$HOME/Library/Fonts/" 2>/dev/null || true
     done
   '');
